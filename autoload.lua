17298f91c1608effdb7295b64e7ce7d65326f744
Potuzhniy Kotik <127777525+basebody@users.noreply.github.com>
2026-09-13 17:06:13 +0300

    Create autoload.lua

---
+++ autoload.lua
@@ -0,0 +1,12 @@
+placeId = game.PlaceId
+
+StealAnEgg = 107778070777162
+MurderDuels = 120851538706364
+MurderMystery2 = 142823291
+
+if placeId == MurderDuels:
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaitofyp/Murder-Duels-Op-Script/refs/heads/main/obf_g6J17E74sja3qYqZlhd15sO8L7T6VK5eBv50l0230S7rSwV9IpoO9n3eUPSy9Jc6.lua.txt"))()
+if placeId == StealAnEgg:
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidShell-null/VoidShell-Hub/refs/heads/main/Scripts/StealAnEgg.luau"))()
+if placeId == MurderMystery2:
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TradeHub-Script/TradeHub-Script/refs/heads/main/loader"))()
